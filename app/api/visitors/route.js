ed6517f4dfe33a792ce0142992a453dc0976ca5a
aditidesignsz <aditidesignsz@gmail.com>
2026-05-01 12:11:10 +0530

    Update route.js

--- app/api/visitors/route.js
+++ app/api/visitors/route.js
@@ -1,8 +1,6 @@
-export const dynamic = 'force-dynamic';
-
 import { NextResponse } from 'next/server';
 import { createClient } from '@supabase/supabase-js';
-
+export const dynamic = 'force-dynamic';
 export async function GET() {
   const supabase = createClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL,
